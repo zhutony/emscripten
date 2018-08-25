3f2b3171656d3a93889a4dfbc32f5b66ef3705b8
Sam Clegg <sbc@chromium.org>
2018-08-24 17:23:38 -0700

    Remove WASM_ASYNC_COMPILATION from the few test cases
    We should prefer async compilation in all tests except those that
    sepcificially test this option.

--- tests/test_core.py
+++ tests/test_core.py
@@ -6280,11 +6280,10 @@ return malloc(size);
 
   ### Integration tests
 
-  @sync
   def test_ccall(self):
     self.emcc_args.append('-Wno-return-stack-address')
     self.set_setting('EXTRA_EXPORTED_RUNTIME_METHODS', ['ccall', 'cwrap'])
-    create_test_file('post.js', '''
+    self.add_pre_run('''
       out('*');
       var ret;
       ret = Module['ccall']('get_int', 'number'); out([typeof ret, ret].join(','));
@@ -6316,7 +6315,6 @@ return malloc(size);
       out('stack is ok.');
       ccall('call_ccall_again', null);
       ''')
-    self.emcc_args += ['--post-js', 'post.js']
 
     self.set_setting('EXPORTED_FUNCTIONS', ['_get_int', '_get_float', '_get_bool', '_get_string', '_print_int', '_print_float', '_print_bool', '_print_string', '_multi', '_pointer', '_call_ccall_again', '_malloc'])
     self.do_run_in_out_file_test('tests', 'core', 'test_ccall')
@@ -6524,14 +6522,12 @@ return malloc(size);
     self.do_run(src, '''waka 4999!''')
     assert '_exported_func_from_response_file_1' in open('src.cpp.o.js').read()
 
-  @sync
   def test_add_function(self):
     self.set_setting('INVOKE_RUN', 0)
     self.set_setting('RESERVED_FUNCTION_POINTERS', 1)
     self.set_setting('EXPORTED_RUNTIME_METHODS', ['callMain'])
     src = path_from_root('tests', 'interop', 'test_add_function.cpp')
-    post_js = path_from_root('tests', 'interop', 'test_add_function_post.js')
-    self.emcc_args += ['--post-js', post_js]
+    self.emcc_args += ['--pre-js', path_from_root('tests', 'interop', 'test_add_function_pre.js')]
 
     print('basics')
     self.do_run_in_out_file_test('tests', 'interop', 'test_add_function')
@@ -7695,7 +7691,6 @@ extern "C" {
     self.emcc_args += ['--pre-js', 'pre.js']
     self.do_run('int main() { return 0; }', 'object\nobject\nobject')
 
-  @sync
   @no_wasm_backend("https://github.com/emscripten-core/emscripten/issues/9039")
   def test_stack_overflow_check(self):
     args = self.emcc_args + ['-s', 'TOTAL_STACK=1048576']
